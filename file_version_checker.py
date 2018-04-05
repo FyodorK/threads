d3e2afde591f88c2245b3b54c228da9358b60aa0
Fyodor Kutsepin <fyodor.kutsepin@gmail.com>
2018-04-05 14:25:14 +0300

    fix bugs, add new

--- file_version_checker.py
+++ file_version_checker.py
@@ -2,6 +2,7 @@ import os
 from win32api import GetFileVersionInfo
 import argparse
 import sys
+import re
 
 __version__ = '0.0.1'
 
@@ -23,16 +24,21 @@ class VersionChecker:
     """
 
     def __init__(self):
-        self.file_to_check = ""
-        self.path_to_check = ""
+
         self.out_mess = []
-        self.template = "{time}\t\tPath to the file: {path:155}\t\t\tProduct version: {version}"
+        self.template = "{path:155}{version:20}{boo}"
         self.checking_extensions = ('exe', 'dll', 'pyd')
+        self.location = os.path.dirname(os.path.realpath(__file__))
+        self.path_to_exception_name_file=os.path.join(self.location, 'exceptions.txt')
 
         args = self.parser_args()
 
         self.file_to_check = args.file
         self.path_to_check = args.folder
+        if args.exceptions:
+            self.path_to_exception_name_file = args.exceptions
+
+        self.exception_names = self.parse_exception_names()
 
     @staticmethod
     def parser_args():
@@ -52,13 +58,21 @@ class VersionChecker:
         group.add_argument('-d', '--folder', metavar='<Folder>', required=False, default=None,
                            help='Provide a folder path that content is needed to be checked')
         parser.add_argument('-v', '--version', action='version', help='Show version', version=__version__)
+        parser.add_argument('-e', '--exceptions', help='Path to file with exceptions')
 
         args = parser.parse_args()
 
         return args
 
+    def parse_exception_names(self):
+        exception_names = []
+        with open(self.path_to_exception_name_file, 'r') as exception_file:
+            for line in exception_file.readlines():
+                exception_names.append(line.strip())
+        return exception_names
+
     @staticmethod
-    def getfileprops(path_to_file):
+    def getfileprops(path_to_file: str) -> dict:
         """
         Get properties of given file
         https://stackoverflow.com/a/7993095/4738630
@@ -106,20 +120,18 @@ class VersionChecker:
 
         return props
 
-    @staticmethod
-    def get_time():
-        return __import__('datetime').datetime.now().strftime("%Y%m%d_%H%M%S.%f")
-
     def gfv_wrapper(self, path_to_file):
 
         filename = path_to_file.split('\\')[-1]
         file_extension = filename.split('.')[-1]
 
         if file_extension in self.checking_extensions:
+            in_exception_list = False
             props = self.getfileprops(path_to_file)
             version = str(props.get('ProductVersion'))
-
-            out = self.template.format(time=self.get_time(), path=path_to_file, version=version)
+            if filename in self.exception_names:
+                in_exception_list = True
+            out = self.template.format(path=path_to_file, version=version, boo=in_exception_list)
             self.out_mess.append(out)
         else:
             pass
@@ -131,22 +143,56 @@ class VersionChecker:
             for folder in [os.path.join(root, fld) for fld in folders]:
                 self.gfv_wrap_folder(folder)
 
-    def post_porn(self):
+    @staticmethod
+    def uniq_sort(cont: list) -> list:
+        """
+        Just a crutch
+        """
+        res = []
+        [res.append(i) for i in cont if not res.count(i)]
+        return res
+
+    def post_process(self):
         """
         Add some statistic to resulting output
         :return: None
         """
-        template = "{h}\nFiles verified: {n}\nFiles have product version: {np}\nFiles have not product version: {na}\n"
-        header = "\n" + "=" * 142
+        top_ = "{path:155}{version:20}{boo:20}".format(path='Path to the file',
+                                                       version='Product version',
+                                                       boo='In exception list')
+        template = {
+            "result": "{h}\nFiles verified: {n}\nFiles have product version: {np}\n"
+                      "Files have not product version: {na}\n",
+            "status": "Overall result: {}",
+            "failed": "Failed objects: {}"
+        }
+
+        header = "=" * 142
         count_none = 0
+        failed_dlls = []
         total_files = len(self.out_mess)
 
         for member in self.out_mess:
-            if 'Product version: None' in member:
+            if re.search(r'None(.+)False', member):
+                failed_dlls.append(member)
+
+        for member in self.out_mess:
+            if ' None ' in member:
                 count_none += 1
 
-        out = template.format(h=header, n=total_files, np=total_files - count_none, na=count_none)
+        out = template.get("result").format(h=header, n=total_files, np=total_files - count_none, na=count_none)
         self.out_mess.append(out)
+
+        count_fails = len(failed_dlls)
+        if count_fails > 0:
+            self.out_mess.append(template.get("status").format("Test Failed"))
+            self.out_mess.append(template.get("failed").format(count_fails))
+            for item in failed_dlls:
+                self.out_mess.append(item)
+        else:
+            self.out_mess.append(template.get("status").format("Test passed"))
+
+        self.out_mess.insert(0, top_)
 
     def main(self):
         if self.file_to_check and os.path.isfile(self.file_to_check):
@@ -154,10 +200,12 @@ class VersionChecker:
         elif self.path_to_check and os.path.isdir(self.path_to_check):
             self.gfv_wrap_folder(self.path_to_check)
         else:
-            out = self.template.format(time=self.get_time(), path='Not available', version='Object is not found')
+            out = "{path:155}{version:20}".format(path='Not available', version='Object is not found')
             self.out_mess.append(out)
 
-        self.post_porn()
+        self.out_mess = self.uniq_sort(self.out_mess)
+
+        self.post_process()
         print('\n'.join(self.out_mess))
 
 
